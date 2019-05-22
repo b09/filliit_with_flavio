f58bc95481d6d397c9b461299c7a1d89ee0b611b
Brendan Prado <bprado@f0r4s21.codam.nl>
2019-05-22 17:31:34 +0200

    correct path added to libft in fillit.h

--- fillit.h
+++ fillit.h
@@ -6,14 +6,14 @@
 /*   By: bprado <bprado@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/05/22 17:15:32 by bprado         #+#    #+#                */
-/*   Updated: 2019/05/22 17:21:23 by bprado        ########   odam.nl         */
+/*   Updated: 2019/05/22 17:31:10 by bprado        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILLIT_H
 # define FILLIT_H
 # include <stdio.h>		// for printf
-# include "libft.h"
+# include "libft/libft.h"
 # include <unistd.h>	// for write / read
 # include <fcntl.h>		// for open
 
